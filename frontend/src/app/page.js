9a7835fd5389e560d3c0cff6223b1f1e7afd06d7
Sanjana <chauhansanjana965@gmail.com>
2025-09-06 00:45:26 +0530

    Pull from main and conflict resolved

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-       
+        
       </main>
     
   );
